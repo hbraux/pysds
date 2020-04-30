b0180515b7b85c77d495b8d8755b1929e2954960
harold.braux <harold.braux@gmail.com>
2020-04-30 11:34:25 +0200

    new project

---
+++ pysds/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__title__ = 'pysds'
+from .version import __version__
+
---
+++ pysds/encoder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Encoder class"""
+
+import sys
+import uuid
+import time
+
+class Encoder(object):
+    """Encoder
+
+    Attributes:
+       tbc
+    """
+
+    def __init__(self):
+        return
+

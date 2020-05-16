f9a1f0e195d75d2b782019f42b1ea3a1788506b5
harold.braux <harold.braux@gmail.com>
2020-05-16 15:51:11 +0200

    test workflow

--- tests/test_config.py
+++ tests/test_config.py
@@ -23,7 +23,7 @@ class TestConfig(unittest.TestCase):
         self.assertTrue(os.path.isfile(TEST_CFG_PATH + CONFIG_FILE))
         self.assertEqual(TEST_DB_URL, config.dburl)
 
-    def test_no_setup(self):
+    def test_setup_done(self):
         config = Config(cfgpath=TEST_CFG_PATH)
         self.assertEqual(TEST_DB_URL, config.dburl)
 
